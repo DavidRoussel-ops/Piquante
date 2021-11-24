8338642fd588a0eb9ffca337ae5a01d71702e3e7
David Roussel <davidroussel171113@outlook.fr>
2021-11-24 21:18:18 +0100

    Ajout de la sécurité

---
+++ backend/Security/token.js
@@ -0,0 +1,22 @@
+
+
+//Constante qui appel jsonwebtoken.
+const jwt = require('jsonwebtoken');
+
+//Vérification du token utilisateur.
+module.exports = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+        const userId = decodedToken.userId;
+        if (req.body.userId && req.body.userId !== userId) {
+            throw 'User ID non valable !';
+        } else {
+            next();
+        }
+    } catch (error) {
+        res.status(401).json({error: new Error('Requete invalide ')});
+    }
+};
+
+
